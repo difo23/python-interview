52d02651bb7891439f63048af77db1c35a8b9a89
BryanISC <39427081+BryanISC@users.noreply.github.com>
2018-06-26 20:29:47 -0400

    Add files via upload

---
+++ PRACTICA-- BRYAN FAJARDO 2018-0325/1-NOMBRE POR PANTALLA.py
@@ -0,0 +1,19 @@
+a = int(input("ingrese un numero"));
+nombre = raw_input("ingrese un nombre");
+contador = int(0);
+ 
+if a <=0:
+  while a <=0:
+    print("error");
+    a = int(input());
+
+print("");
+while (a > contador):
+  print (nombre);
+  print("");
+  contador += 1
+
+print ("programa finalizado");
+
+
+
---
+++ PRACTICA-- BRYAN FAJARDO 2018-0325/4-MAXVAL.py
@@ -0,0 +1,16 @@
+a = int(input("Introduce el valor n "))
+b = int(input("Introduce el valor m "))
+
+
+def maxVal(n, m):
+
+    if n > m:
+        print(" El mayor valor es n %d " % n)
+    else:
+        print(" El mayor valor es m %d " % m)
+
+
+maxVal(a, b)
+
+
+
